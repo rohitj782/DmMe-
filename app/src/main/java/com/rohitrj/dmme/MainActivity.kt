a64ad1bea8aca14410faddba479a1539f1c04126
rohitj782 <rohitj782@gmail.com>
2019-08-01 07:17:49 +0530

    initial commit

--- app/src/main/java/com/rohitrj/dmme/MainActivity.kt
+++ app/src/main/java/com/rohitrj/dmme/MainActivity.kt
@@ -2,11 +2,19 @@ package com.rohitrj.dmme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.analytics.FirebaseAnalytics
+
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mFirebaseAnalytics: FirebaseAnalytics? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtain the FirebaseAnalytics instance.
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this)
     }
 }
